040fe64b662d649811cd67fcfe72db3701950ee2
Guilherme Fleiger Felipe <guilhermefliegerfelipe@gmail.com>
2021-05-26 21:05:29 -0300

    Adicionando prettier

--- src/managers/key_manager.ts
+++ src/managers/key_manager.ts
@@ -1,24 +1,22 @@
 export default {
     _keys: Array<string>(),
 
-    add_key( key: string ) {
-        switch( key )
-        {
+    add_key(key: string) {
+        switch (key) {
             case 'ArrowRight':
             case 'ArrowLeft':
             case 'ArrowDown':
             case 'ArrowUp':
-                if( this._keys.indexOf( key ) === -1 )
-                {
-                    this._keys.push( key );
+                if (this._keys.indexOf(key) === -1) {
+                    this._keys.push(key);
                 }
                 break;
         }
     },
-    pop_key( key: string ) {
-        this._keys = this._keys.filter( val => val != key );
+    pop_key(key: string) {
+        this._keys = this._keys.filter((val) => val != key);
     },
-    forEach( cb: ( key: string ) => void ) {
-        this._keys.forEach( cb );
+    forEach(cb: (key: string) => void) {
+        this._keys.forEach(cb);
     }
-}
+};
