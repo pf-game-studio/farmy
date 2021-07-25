dba65d25fe47be2216ae83f56365a094d6fc11db
Guilherme Fleiger Felipe <guilhermefliegerfelipe@gmail.com>
2021-07-25 15:04:57 -0300

    Finalizando a troca de mídia entre dois clientes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,5 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import { io, httpServer } from './io';
 import connection_controller from './controllers/connection_controller';
-
-const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, {
-    cors: {
-        origin: 'http://localhost:5000',
-        methods: ['GET', 'POST']
-    }
-});
 
 io.on('connection', connection_controller.on_connection);
 
